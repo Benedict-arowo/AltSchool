96625c49f8a882665a845443abf6407333ff45bf
Benedict <benedict.arowo@gmail.com>
2024-01-15 16:19:37 +0300

    simple server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+const http = require("http");
+
+const PORT = 8900;
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { "Content-Type": "application/json" });
+	res.end("Benedict Arowojolu-Alagwe");
+});
+
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
