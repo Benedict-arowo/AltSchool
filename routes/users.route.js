a0b1284aff2c8394e728152d9739005303c1eeac
Benedict <benedict.arowo@gmail.com>
2024-03-27 11:28:33 +0300

    Update package.json and users.route.js

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const {
-	createUser,
-	getUsers,
-	loginUser,
-} = require("../controllers/user.controller");
+const { createUser, loginUser } = require("../controllers/user.controller");
 const userRouter = express.Router();
 const wrapper = require("../middlewear/wrapper");
 
-userRouter.route("/").get(wrapper(getUsers)).post(wrapper(createUser));
+userRouter.route("/register").post(wrapper(createUser));
 userRouter.route("/login").post(wrapper(loginUser));
 
 module.exports = userRouter;
